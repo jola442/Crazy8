4d995f0666fa5becbc8c8d1710e73fa710e163b5
Jola Ajayi <benjamen.ajayi@gmail.com>
2022-12-01 16:47:52 -0500

    UI - added code for the cards component

--- frontend/src/components/Cards/index.js
+++ frontend/src/components/Cards/index.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import Card from '../Card';
+import "./index.css"
 
-function Cards() {
-  return (
-    <div>Cards</div>
-  )
+
+function Cards( {cardsList, toggleSelectedCard} ) {
+  return <ul>
+    <li className='cardsList'>
+      {cardsList.map( card => (
+      <Card key={card.id} card={{...card}} toggleSelectedCard={toggleSelectedCard} selected={card.selected}></Card>))}
+    </li>
+  </ul>
 }
 
 export default Cards
