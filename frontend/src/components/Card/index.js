2fed4195c17f3ed44d72c33c201a4dd4c14ecb85
Jola Ajayi <benjamen.ajayi@gmail.com>
2022-12-01 16:46:27 -0500

    UI - added code and styles for the card component

--- frontend/src/components/Card/index.js
+++ frontend/src/components/Card/index.js
@@ -1,8 +1,58 @@
-import React from 'react'
+import "./index.css"
 
-function Card() {
+function Card( {card, selected, toggleSelectedCard} ) {
+
+  const getCardImage = () => {
+    let url = "/images/"
+    if(!card){
+        url += "back_card/blue.png";
+        return url;
+    }
+
+    if (card.front){
+        url += "front_cards/" + card.suit + "_" + card.rank + ".png";
+    }
+
+    else{
+      url += "back_card/blue.png";
+  }
+    return url;
+  }
+
+  const getClassName = () => {
+    let newClassName;
+    if(!card){
+      newClassName = "stockpile card";
+    }
+
+    else{
+      newClassName = card.rank + "-" + card.suit + " card";
+
+      if(selected){
+        newClassName+= " active";
+      }
+    }
+
+    return newClassName;
+
+  }
+
+
+  function handleCardClicked(){;
+    if(card){
+        toggleSelectedCard(card.id);
+    }   
+  
+  }
+
   return (
-    <div>index</div>
+    <>
+      <div className={getClassName()} onClick={handleCardClicked}>
+        <img src={getCardImage()} alt={card?card.rank + " " + card.suit:"stockpile"}></img>
+      </div>
+
+    </>
+
   )
 }
 
