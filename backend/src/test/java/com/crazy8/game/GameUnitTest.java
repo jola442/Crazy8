df3b6ece2fc09f13000e1cb68db0aa09032f3f22
Jola Ajayi <benjamen.ajayi@gmail.com>
2022-12-02 16:20:24 -0500

    U-TEST - added a test for drawing cards

---
+++ backend/src/test/java/com/crazy8/game/GameUnitTest.java
@@ -0,0 +1,22 @@
+package com.crazy8.game;
+
+import org.junit.jupiter.api.Test;
+import com.crazy8.game.Defs.*;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameUnitTest {
+    Game game = new Game();
+    @Test
+    public void testDrawCard(){
+        for(int i = 0; i < Defs.NUM_CARDS; ++i){
+            game.drawCard();
+        }
+        assertEquals(0, game.getNumClubsCards());
+        assertEquals(0, game.getNumDiamondsCards());
+        assertEquals(0, game.getNumHeartsCards());
+        assertEquals(0, game.getNumSpadesCards());
+        assertEquals(0, game.getNumCards());
+    }
+}
