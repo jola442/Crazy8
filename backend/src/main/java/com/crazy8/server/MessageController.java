61ac828c732591ca3aae91613709f92da590bbd4
Jola Ajayi <benjamen.ajayi@gmail.com>
2022-12-02 07:41:46 -0500

    NETWORK - created and added code for the MessageController class

--- backend/src/main/java/com/crazy8/server/MessageController.java
+++ backend/src/main/java/com/crazy8/server/MessageController.java
@@ -1,4 +1,44 @@
 package com.crazy8.server;
+import com.crazy8.game.Player;
+import com.crazy8.game.Game;
+import com.crazy8.server.Defs.Action;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+import java.util.ArrayList;
+import java.util.List;
 
+@Controller
 public class MessageController {
+
+    @Autowired
+    private SimpMessagingTemplate simpMessagingTemplate;
+
+    private List<Player> players = new ArrayList<>();
+
+    @MessageMapping("/message")  //app/message
+    @SendTo("/playroom/public")
+    private ServerMessage receivePublicMessage(@Payload ClientMessage message){
+        ServerMessage response = new ServerMessage();
+        if(message.getAction() == Action.JOIN){
+            Player player = new Player(message.getName(), Integer.toString(players.size()+1) );
+            players.add(player);
+            response.setId(player.getId());
+        }
+        response.setMessage("Hello all");
+
+        return response;
+    }
+
+    @MessageMapping("/private-message")
+    public ServerMessage receivePrivateMessage(@Payload ClientMessage message){
+        ServerMessage response = new ServerMessage();
+        simpMessagingTemplate.convertAndSendToUser(message.getName(), "/private", message); //user/David/private
+        response.setMessage("Hello " + message.getName());
+        return response;
+    }
+
 }
