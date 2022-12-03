fca6d707efa098cd216efbd6ad744d2312ad93b5
Jola Ajayi <benjamen.ajayi@gmail.com>
2022-12-03 11:54:20 -0500

    U-TEST - added a test for starting the game

--- backend/src/test/java/com/crazy8/game/GameUnitTest.java
+++ backend/src/test/java/com/crazy8/game/GameUnitTest.java
@@ -2,6 +2,8 @@ package com.crazy8.game;
 
 import org.junit.jupiter.api.Test;
 import com.crazy8.game.Defs.*;
+
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -24,5 +26,25 @@ class GameUnitTest {
     public void testPlaceStartingCard(){
         Card startingCard = game.placeStartingCard();
         assertFalse(startingCard.getRank()==Rank.EIGHT);
+    }
+
+    @Test
+    public void testStartGame(){
+        Player player1 = new Player("Player 1");
+        Player player2 = new Player("Player 2");
+        Player player3 = new Player("Player 3");
+        Player player4 = new Player("Player 4");
+        game.getPlayers().addAll(Arrays.asList(player1, player2, player3, player4));
+        game.start();
+        assertFalse(game.getTopCard()==null);
+        assertFalse(player1.getHand().contains(null));
+        assertFalse(player2.getHand().contains(null));
+        assertFalse(player3.getHand().contains(null));
+        assertFalse(player4.getHand().contains(null));
+        assertEquals(5, player1.getHand().size());
+        assertEquals(5, player2.getHand().size());
+        assertEquals(5, player3.getHand().size());
+        assertEquals(5, player4.getHand().size());
+        assertEquals(31, game.getNumCards());
     }
 }
