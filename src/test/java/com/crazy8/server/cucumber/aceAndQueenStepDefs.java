28cc0100a7ed0dfee6f0b381a23be5680cdc97d3
Jola Ajayi <benjamen.ajayi@gmail.com>
2022-12-08 21:30:41 -0500

    CODE - added code for ace and queen step definitions

--- src/test/java/com/crazy8/server/cucumber/aceAndQueenStepDefs.java
+++ src/test/java/com/crazy8/server/cucumber/aceAndQueenStepDefs.java
@@ -29,7 +29,7 @@ import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOf;
 
 @SpringBootTest
 @DirtiesContext
-public class aceAndQueenSinglePlayerStepDefs {
+public class aceAndQueenStepDefs {
 
     private static final ArrayList<WebDriver> webDrivers = new ArrayList<>();
     private static final String PORT_URL = "http://127.0.0.1:8080";
@@ -43,16 +43,17 @@ public class aceAndQueenSinglePlayerStepDefs {
 
     private static final Card topCardOne = new Card(Rank.EIGHT, Suit.CLUBS);
 
-//    private static final Card topCardTwo = new Card(Rank.EIGHT, Suit.HEARTS);
+    //    private static final Card topCardTwo = new Card(Rank.EIGHT, Suit.HEARTS);
     private static final ArrayList<Card> playerOneHand =
             new ArrayList<>(Arrays.asList(
                     //needed for rigging
                     new Card(Rank.THREE, Suit.CLUBS),
                     new Card(Rank.ACE, Suit.HEARTS),
                     new Card(Rank.QUEEN, Suit.CLUBS),
+                    new Card(Rank.THREE, Suit.HEARTS),
                     //not needed for rigging
-                    new Card(Rank.TWO, Suit.CLUBS),
-                    new Card(Rank.THREE, Suit.HEARTS)
+                    new Card(Rank.TWO, Suit.CLUBS)
+
             )
 
             );
@@ -60,11 +61,12 @@ public class aceAndQueenSinglePlayerStepDefs {
     private static final ArrayList<Card> playerTwoHand =
             new ArrayList<>(Arrays.asList(
                     new Card(Rank.FOUR, Suit.CLUBS),
+                    new Card(Rank.TWO, Suit.HEARTS),
+                    new Card(Rank.FOUR, Suit.HEARTS),
                     //not needed for rigging
                     new Card(Rank.ACE, Suit.SPADES),
-                    new Card(Rank.TWO, Suit.SPADES),
-                    new Card(Rank.THREE, Suit.SPADES),
-                    new Card(Rank.FOUR, Suit.SPADES)
+                    new Card(Rank.THREE, Suit.SPADES)
+
             )
 
             );
@@ -74,9 +76,9 @@ public class aceAndQueenSinglePlayerStepDefs {
                     //needed for rigging
                     new Card(Rank.SEVEN, Suit.HEARTS),
                     new Card(Rank.FIVE, Suit.CLUBS),
+                    new Card(Rank.THREE, Suit.HEARTS),
+                    new Card(Rank.FIVE, Suit.HEARTS),
                     //not needed for rigging
-                    new Card(Rank.ACE, Suit.DIAMONDS),
-                    new Card(Rank.TWO, Suit.DIAMONDS),
                     new Card(Rank.THREE, Suit.DIAMONDS)
             )
 
@@ -84,10 +86,12 @@ public class aceAndQueenSinglePlayerStepDefs {
 
     private static final ArrayList<Card> playerFourHand =
             new ArrayList<>(Arrays.asList(
+                    //needed for rigging
                     new Card(Rank.QUEEN, Suit.CLUBS),
-                    new Card(Rank.NINE, Suit.SPADES),
-                    new Card(Rank.ACE, Suit.CLUBS),
+                    new Card(Rank.ACE, Suit.HEARTS),
                     new Card(Rank.THREE, Suit.CLUBS),
+                    //Not needed for rigging
+                    new Card(Rank.SEVEN, Suit.HEARTS),
                     new Card(Rank.FOUR, Suit.CLUBS)
             )
 
@@ -95,10 +99,6 @@ public class aceAndQueenSinglePlayerStepDefs {
 
     @Autowired
     private Game game;
-
-    public WebElement waitForDisplayed(WebDriver webDriver, By selector) {
-        return new WebDriverWait(webDriver, Duration.ofSeconds(10)).until(visibilityOf(webDriver.findElement(selector)));
-    }
 
     public boolean hasChildren(WebElement webElement) {
         return webElement.findElements(By.xpath("./descendant-or-self::*")).size() > 1;
@@ -119,11 +119,15 @@ public class aceAndQueenSinglePlayerStepDefs {
     }
 
 
-    @Given("all players are connected")
-    public void allPlayersAreConnected() throws InterruptedException {
+    @Given("all players are connected and the starting card is {}")
+    public void allPlayersAreConnected(String startingCardString) throws InterruptedException {
         game.resetState();
         ArrayList<Card> riggedCards = new ArrayList<>();
-        game.setTopCard(topCardOne);
+        Rank cardRank = Rank.valueOf(startingCardString.split("-")[0]);
+        Suit cardSuit = Suit.valueOf(startingCardString.split("-")[1]);
+        Card startingCard = new Card(cardRank, cardSuit);
+        System.out.println("Card i created in the test method: " + startingCard);
+        game.setTopCard(startingCard);
         riggedCards.addAll(playerOneHand);
         riggedCards.addAll(playerTwoHand);
         riggedCards.addAll(playerThreeHand);
@@ -133,85 +137,93 @@ public class aceAndQueenSinglePlayerStepDefs {
         riggedDeck.setCards(riggedCards);
         game.setDeck(riggedDeck);
         WebDriverManager.chromedriver().setup();
-        for(int i = 0; i < 4; ++i) {
+        for (int i = 0; i < 4; ++i) {
             webDrivers.add(new ChromeDriver());
             WebDriver webDriver = webDrivers.get(i);
+            webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
             webDriver.get(PORT_URL);
             webDriver.manage().window().maximize();
-            waitForDisplayed(webDriver, NAME_TEXTBOX);
             webDriver.findElement(NAME_TEXTBOX).sendKeys("Player " + Integer.toString(i + 1));
-            waitForDisplayed(webDriver, JOIN_BUTTON);
             webDriver.findElement(JOIN_BUTTON).click();
-            webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(1));
-
         }
 
-
-
-        for(int i = 0; i < 4; ++i){
-            webDrivers.get(i).manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-//            new WebDriverWait(webDrivers.get(i), Duration.ofSeconds(30)).until(elementToBeClickable(webDrivers.get(3).findElement(By.cssSelector("ul.cardsList.li.card"))));
-        }
+        webDrivers.get(3).manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
         assertEquals("LEFT", webDrivers.get(3).findElement(CURRENT_GAME_DIRECTION).getText());
     }
 
 
-    @When("player {} plays {} on {}")
-    public void playerPlaysCard(int playerNum, String cardString, String topCardString) throws InterruptedException {
-        if(playerNum == 1){
-//            Rank topCardRank = Rank.values()[Integer.parseInt(topCardString.split("-")[0].trim())-1];
-//            Suit topCardSuit = Suit.valueOf(topCardString.split("-")[1]);
-//            game.setTopCard(new Card(topCardRank, topCardSuit));
+    @When("player {} plays {}")
+    public void playerPlaysCard(int playerNum, String cardString) throws InterruptedException {
+        if (playerNum == 1) {
             WebDriver webDriver = webDrivers.get(0);
             cardString = cardString.toLowerCase();
-
-//            webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
-            waitForDisplayed(webDriver,By.className(cardString));
             webDriver.findElement(By.className(cardString)).click();
             WebElement playCardButton = webDriver.findElement(PLAY_CARD_BUTTON);
             playCardButton.click();
-            webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
             WebElement topCard = webDriver.findElement(By.xpath("//body/div[@id='root']/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]"));
-            System.out.println("TopCard Element: " + topCard.getAttribute("innerHTML"));
+            System.out.println("TEST: Current top card " + topCard.getAttribute("innerHTML"));
             System.out.println(cardString);
+
             assertTrue(hasClass(topCard, cardString));
-        }
+        } else {
+            if (playerNum == 4) {
+                ArrayList<String> playerCardSelections = new ArrayList<String>();
+                if (game.getTopCard().equals(new Card(Rank.EIGHT, Suit.CLUBS))) {
+                    playerCardSelections.addAll(Arrays.asList("3-clubs", "4-clubs", "5-clubs"));
+                } else {
+                    playerCardSelections.addAll(Arrays.asList("3-hearts", "4-hearts", "5-hearts"));
+                }
 
-        else{
-            ArrayList<String> playerCardSelections = new ArrayList<>(Arrays.asList("3-clubs", "4-clubs", "5-clubs"));
 
-            for(int i = 0; i < playerCardSelections.size(); ++i){
-                WebDriver webDriver = webDrivers.get(i);
-                String playerCardSelection = playerCardSelections.get(i);
-//                webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
-                waitForDisplayed(webDriver, (By.className(playerCardSelection)));
-                webDriver.findElement(By.className(playerCardSelection)).click();
+                for (int i = 0; i < playerCardSelections.size(); ++i) {
+                    WebDriver webDriver = webDrivers.get(i);
+                    String playerCardSelection = playerCardSelections.get(i);
+                    webDriver.findElement(By.className(playerCardSelection)).click();
+                    WebElement playCardButton = webDriver.findElement(PLAY_CARD_BUTTON);
+                    playCardButton.click();
+                    System.out.println("TEST: PLAYER " + (i+1) + " playing " + playerCardSelection);
+                }
+
+                WebDriver webDriver = webDrivers.get(3);
+                cardString = cardString.toLowerCase();
+
+                System.out.println("TEST: PLAYER 4 playing " + cardString);
+                webDriver.findElement(By.className(cardString)).click();
                 WebElement playCardButton = webDriver.findElement(PLAY_CARD_BUTTON);
                 playCardButton.click();
+
+                WebElement topCard = webDriver.findElement(By.xpath("//body/div[@id='root']/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]"));
+
+                System.out.println("TEST: Top card is currently: " + topCard.getAttribute("class"));
+//                System.out.println("TEST: Checking if this was played: " + cardString);
+//            Thread.sleep(5000);
+                assertTrue(hasClass(topCard, cardString));
+            } else {
+                if (playerNum == 3) {
+                    WebDriver webDriver = webDrivers.get(2);
+                    cardString = cardString.toLowerCase();
+                    webDriver.findElement(By.className(cardString)).click();
+                    WebElement playCardButton = webDriver.findElement(PLAY_CARD_BUTTON);
+                    playCardButton.click();
+                    System.out.println("TEST: PLAYER " + 3 + " playing " + cardString);
+                    WebElement topCard = webDriver.findElement(By.xpath("//body/div[@id='root']/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]"));
+
+                    System.out.println("TEST: The top card is currently: " + topCard.getAttribute("class"));
+                    System.out.println("TEST: Checking if this was played: " + cardString);
+//            Thread.sleep(5000);
+                    assertTrue(hasClass(topCard, cardString));
+                }
             }
 
-            WebDriver webDriver = webDrivers.get(3);
-            cardString = cardString.toLowerCase();
-
-//            webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
-            waitForDisplayed(webDriver, (By.className(cardString)));
-            webDriver.findElement(By.className(cardString)).click();
-            WebElement playCardButton = webDriver.findElement(PLAY_CARD_BUTTON);
-            playCardButton.click();
-//            webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-            waitForDisplayed(webDriver, By.xpath("//body/div[@id='root']/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/img[1]"));
-            WebElement topCard = webDriver.findElement(By.xpath("//body/div[@id='root']/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]"));
-            System.out.println("TopCard Element: " + topCard.getAttribute("innerHTML"));
-            System.out.println("Card class i am looking for: " +cardString);
-            assertTrue(hasClass(topCard, cardString));
         }
 
     }
 
-    @Then("{} should play next")
-    public void shouldPlayNext(int nextPlayer) {
+    @Then("player {} should play next")
+    public void shouldPlayNext(int nextPlayer) throws InterruptedException {
         WebDriver webDriver = webDrivers.get(3);
-        webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+//        webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        Thread.sleep(5000);
         WebElement currentTurn = webDriver.findElement(CURRENT_TURN);
         assertEquals("Player " + Integer.toString(nextPlayer), currentTurn.getText());
     }
@@ -219,10 +231,8 @@ public class aceAndQueenSinglePlayerStepDefs {
     @And("the game direction is now {}")
     public void theGameDirectionIsNow(Direction newDirection) {
         WebDriver webDriver = webDrivers.get(3);
-        webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+//        webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         WebElement currentGameDirection = webDriver.findElement(CURRENT_GAME_DIRECTION);
         assertEquals(newDirection.toString(), currentGameDirection.getText());
     }
-
-
 }
